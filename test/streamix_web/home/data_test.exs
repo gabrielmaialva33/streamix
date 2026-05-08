3e032914127d65e6651392a53cdb3d558d1678e6
Gabriel Maia <gabrielmaialva33@gmail.com>
2026-05-08 20:35:47 -0300

    test(home): add tests for `toggle_content_favorite/3` to manage favorites and update home state

---
+++ test/streamix_web/home/data_test.exs
@@ -0,0 +1,67 @@
+defmodule StreamixWeb.Home.DataTest do
+  use Streamix.DataCase, async: true
+
+  import Streamix.AccountsFixtures
+  import Streamix.IptvFixtures
+
+  alias Streamix.Iptv
+  alias StreamixWeb.Home.Data
+
+  describe "toggle_content_favorite/3" do
+    test "persists movie favorites from home card previews and refreshes home state" do
+      user = user_fixture()
+      provider = provider_fixture(user)
+      movie = movie_fixture(provider, %{title: "Preview Favorite"})
+
+      socket = home_socket(user, featured: {:movie, movie})
+
+      refute Iptv.is_favorite?(user.id, "movie", movie.id)
+
+      socket = Data.toggle_content_favorite(socket, "movie", Integer.to_string(movie.id))
+
+      assert Iptv.is_favorite?(user.id, "movie", movie.id)
+      assert MapSet.member?(socket.assigns.movie_favorites_map, movie.id)
+      assert socket.assigns.featured_favorite
+
+      assert Enum.any?(socket.assigns.favorites, fn favorite ->
+               favorite.content_type == "movie" and favorite.content_id == movie.id
+             end)
+    end
+
+    test "removes existing series favorites from home card previews" do
+      user = user_fixture()
+      provider = provider_fixture(user)
+      series = series_content_fixture(provider, %{title: "Favorited Series"})
+
+      {:ok, _favorite} = Iptv.add_favorite(user.id, "series", series.id)
+
+      socket =
+        home_socket(user,
+          featured: {:series, series},
+          series_favorites_map: MapSet.new([series.id])
+        )
+
+      socket = Data.toggle_content_favorite(socket, :series, series.id)
+
+      refute Iptv.is_favorite?(user.id, "series", series.id)
+      refute MapSet.member?(socket.assigns.series_favorites_map, series.id)
+      refute socket.assigns.featured_favorite
+    end
+  end
+
+  defp home_socket(user, attrs) do
+    assigns =
+      %{
+        __changed__: %{},
+        current_scope: user_scope_fixture(user),
+        featured: nil,
+        favorites: [],
+        featured_favorite: false,
+        movie_favorites_map: MapSet.new(),
+        series_favorites_map: MapSet.new()
+      }
+      |> Map.merge(Map.new(attrs))
+
+    %Phoenix.LiveView.Socket{assigns: assigns}
+  end
+end
